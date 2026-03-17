d6467b4671837e05a4610feb10980981f99a14f2
Maheshvar V <maheshvarkv007@gmail.com>
2026-03-17 21:57:34 +0530

    Time: 7 ms (84.31%), Space: 14.4 MB (54.84%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,16 @@
+class Solution(object):
+   def topKFrequent(self,nums, k):
+    freq = {}
+    for num in nums:
+        if num in freq:
+            freq[num] += 1
+        else:
+            freq[num] = 1
+    sorted_items = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+    result = []
+    for i in range(k):
+        result.append(sorted_items[i][0])
+    return result
+
+
+        
