7c78c67184f982b13ca81a280a75874d4e041313
Maheshvar V <maheshvarkv007@gmail.com>
2026-04-03 18:36:30 +0530

    Time: 3 ms (12.06%), Space: 13.4 MB (21.7%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def search(self,nums, target):
+        l, h = 0, len(nums) - 1
+        while l <= h:
+            m = (l + h) // 2
+            if nums[m] == target: return m
+            elif nums[m] < target: l = m + 1
+            else: h = m - 1
+        return -1
