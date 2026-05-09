d5fde512746f0b4c9566d4f3423e8382cc13895e
Maheshvar V <maheshvarkv007@gmail.com>
2026-05-09 22:57:33 +0530

    Time: 30 ms (21.9%), Space: 12.5 MB (57.37%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,8 +1,3 @@
 class Solution(object):
     def nextGreaterElement(self, nums1, nums2):
-        st, d = [], {}
-        for n in nums2:
-            while st and st[-1] < n:
-                d[st.pop()] = n
-            st.append(n)
-        return [d.get(x, -1) for x in nums1]
+        return [next((y for y in nums2[nums2.index(x)+1:] if y>x), -1) for x in nums1]
