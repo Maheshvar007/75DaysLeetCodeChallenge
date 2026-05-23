f750e09b66566c1b1f77a39833d7fadacfedd24e
Maheshvar V <maheshvarkv007@gmail.com>
2026-05-23 15:41:29 +0530

    Time: 0 ms (100%), Space: 12.4 MB (16.24%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        while len(stones) > 1:
+            stones.sort()
+            y = stones.pop() 
+            x = stones.pop()
+            if x != y:
+                stones.append(y - x)
+        return stones[0] if stones else 0
